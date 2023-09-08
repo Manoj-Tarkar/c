4583c7635061add9ccc06f1358c29c476fb4acec
Manoj Tarkar <143936784+Manoj-Tarkar@users.noreply.github.com>
2023-09-08 21:59:15 +0530

    Add files via upload

---
+++ manojtarkar.cpp3.cpp
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int m,a,b;
+	printf("enter the no");
+	scanf("%d%d",&a,&b);
+	m=a;
+	if(m<b)
+	m=b;
+	printf("enter the greater value=%d",m);
+return 0;	
+}
+
+int main()
+{
+	int a;
+	printf("enter the no");
+	scanf("%d",&a);
+	if(a>=0)
+	printf("enter the positive");
+	else
+	printf("enter the negetive");
+	return 0;
+}
+
+int main()
+{
+	int a;
+	printf("enter the no");
+	scanf("%d",&a);
+	if(a%2==0)
+	printf("enter the even");
+	else
+	printf("enter the odd");
+	return 0;
+}
+
+main()
+{
+	float f,c;
+	{
+		printf("enter fahrenhit put this value=");
+		scanf("%f",&f);
+		if(c=(f-32)*5/9)
+		{
+			printf("value of the celsius no=%f",c);
+		}
+	}
+}
+
+
+ main()
+ {
+ 	int a,b,c;
+ 	printf("enter the value=");
+ 	scanf("%d%d",&c,&a);
+ 	if(c==1)
+ 	{
+ 		b=a*a;
+ 	
+ 		printf("enter the square no=%d",b);
+ }
+ 	else
+ 	{
+ 		b=(a*a*a);
+    printf("enter the cube no=%d",b);
+
+	 }
+ }
+
